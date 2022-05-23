41664f83f635619c573e1b7134bbd747501cb5d1
Josep Viciana <emmgfx@gmail.com>
2022-05-23 15:59:44 +0200

    metadata

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,11 @@ export default function Home() {
           name="description"
           content="Understand how you money can fly away with a mortgage and how much relevant are the periodical amortizations"
         />
-        <meta property="og:image" content="/screenshot.png" />
+        <meta property="og:title" content="RMORT - Understand your mortgage" />
+        <meta
+          property="og:image"
+          content="https://rmort.vercel.app/screenshot.png"
+        />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@emmgfx" />
