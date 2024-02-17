1038dff807002624aa8e5b0e25a3bb6acf4e0dc6
Josep Viciana <emmgfx@gmail.com>
2024-02-17 15:55:34 +0100

    Zustand form store

---
+++ store/form.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+export const useFormStore = create((set) => ({
+  months: 0,
+  capital: 0,
+  tae: 0,
+  startDate: new Date().toISOString().substring(0, 10),
+  vpaAmount: 0,
+  vpaInterval: 0,
+  setMonths: (months) => set({ months }),
+  setCapital: (capital) => set({ capital }),
+  setTae: (tae) => set({ tae }),
+  setStartDate: (startDate) => set({ startDate }),
+  setVpaAmount: (vpaAmount) => set({ vpaAmount }),
+  setVpaInterval: (vpaInterval) => set({ vpaInterval }),
+}));
